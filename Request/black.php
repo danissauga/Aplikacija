704f12aa10882ff0b0e25fba70b73eab1d855904
ingabesus <ingabesus@gmail.com>
2021-12-11 14:21:38 +0200

    Update black.php

--- Request/black.php
+++ Request/black.php
@@ -5,20 +5,22 @@
 </head>
 <?php
 
+  $skaicius = rand(3,10);
+  $raides = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  for ($i=0; $i<$skaicius; $i++){
+    ?> <input type="checkbox" id="letter" name="letters" value=""> <?php 
+    foreach ($raides as $letters ){
+        
+    }
+  }
+   
   
-    
 ?>
 <body style="background-color:black">
-<form id="a" name="a" action="" method="POST">
-    <?php
-    for ($i=0, $i<10, $i++)
-
-    ?>
+<form id="a" name="a" action="" method="POST"> 
 <input type="hidden" name="go" value="1"/>
-<button  type="submit">ą</button>
+<button  type="submit">Siusti</button>
 </form>
-
-
 
 </body>
 </html>
