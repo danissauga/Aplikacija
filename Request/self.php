b3678a03e5f540189869b857f6ed4505ac6342b0
ingabesus <ingabesus@gmail.com>
2021-12-11 10:01:07 +0200

    Update self.php

--- Request/self.php
+++ Request/self.php
@@ -9,32 +9,22 @@
 <head>
 <title>Hello!</title>
 </head>
-
-
 <?php
 //if ( (isset($_GET['color']) ) and ( $_GET['color'] == 1) ) {$color = "red"; } else { $color= "black"; }
-
 
 if (isset($_GET['color'])) {
 $color = $_GET['color'];
 }
 
-
-
 ?>
 <body style="background-color:#<?php echo $color; ?>">
-
 <form id="a" name="a" action="" method="GET">
 <input id="color" name="color" placeholder="Iveskite spalva">
 <input id="priskirti" name="priskirti" type="submit" value="Priskirti spalva">
 </form>
 
-
-
 <a href="self.php">Be kintamojo </a><br>
 <!--<a href="self.php?color=1">Su kintamuoju </a>-->
-
-
 
 </body>
 </html>
