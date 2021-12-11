c39eb5a795b0f0d7090c8e779a2d452bc4de600e
ingabesus <ingabesus@gmail.com>
2021-12-11 14:01:13 +0200

    Create black.php

---
+++ Request/black.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Inga</title>
+</head>
+<?php
+
+  
+    
+?>
+<body style="background-color:black">
+<form id="a" name="a" action="" method="POST">
+    <?php
+    for ($i=0, $i<10, $i++)
+
+    ?>
+<input type="hidden" name="go" value="1"/>
+<button  type="submit">ą</button>
+</form>
+
+
+
+</body>
+</html>
